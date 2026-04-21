4349ec4d7b8e7de5388c0b8fc395f17c267c8975
Filip Majchrak <256346@vutbr.cz>
2026-04-21 13:28:19 +0200

    Initial clean structure refactor

--- scripts/editor.js
+++
@@ -1,6 +0,0 @@
-window.editor = CodeMirror.fromTextArea(document.getElementById("editor"), 
-{
-    lineNumbers: true,
-    mode: "text/x-csrc",
-    theme: "default"
-});
--- scripts/view_switch.js
+++
@@ -1,7 +0,0 @@
-// Prepínanie pohľadov
-document.querySelectorAll('.nav-tab').forEach(link => {
-    link.addEventListener('click', () => {
-    document.querySelectorAll('.view').forEach(v => v.classList.remove('active'));
-    document.getElementById(link.dataset.target).classList.add('active');
-    });
-});
